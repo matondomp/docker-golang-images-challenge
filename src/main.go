7c4195ae42b32841183792e317d2529b045559ac
matondo antonio <matondo.antonio@ideiasdinamicas.com>
2022-12-13 00:18:30 +0100

    updatet docker file

--- src/main.go
+++ src/main.go
@@ -1,21 +1,9 @@
 
+
 package main
 
-import (
-    "fmt"
-    "log"
-    "net/http"
-)
-
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Full Cycle Rocks!!")
-}
-
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":10000", nil))
-}
+import "fmt"
 
 func main() {
-    handleRequests()
+	fmt.Println("Hello, World!")
 }
